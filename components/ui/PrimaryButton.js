b6266c69a438c4c84a81be40676f3be579048d19
Matthew <matvei.bohush@gmail.com>
2024-08-16 18:14:47 +0300

    add game logic

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { Text, View, Pressable, StyleSheet } from 'react-native'
+import Colors from '../../constants/colors'
 
 function PrimaryButton({ children, onPress }) {
   function pressHandler() {}
@@ -11,7 +12,7 @@ function PrimaryButton({ children, onPress }) {
             : styles.buttonInnerContainer
         }
         onPress={onPress}
-        android_ripple={{ color: '#640233' }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -28,7 +29,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   buttonInnerContainer: {
-    backgroundColor: '#72063c',
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
-import Title from '../components/Title'
+import Title from '../components/ui/Title'
+import Colors from '../constants/colors'
+import { useState } from 'react'
+import NumberContainer from '../components/game/NumberContainer'
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude)
+  } else {
+    return rndNum
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text></Text>
       </View>
@@ -23,10 +40,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#ddb52f',
+    color: Colors.accent500,
     textAlign: 'center',
     borderWidth: 2,
-    borderColor: '#ddb52f',
+    borderColor: Colors.accent500,
     padding: 12,
   },
 })
